c8c3135bdfe1bcc42b52744b397f387dba6a71d1
mhnagano <michel.nagano@gmail.com>
2018-05-05 15:37:41 -0300

    add runnable karma script

---
+++ tests/controllerTest.js
@@ -0,0 +1,25 @@
+describe('SampleController', function(){
+
+  it('should do something', function(){
+    expect('hello').toBe('hello');
+  });
+
+  // beforeEach(module('app'));
+  //
+  // let $controller;
+  //
+  // beforeEach(inject(function(_$controller_){
+  //   $controller = _$controller_;
+  // }));
+  //
+  // describe('sum', function(){
+  //   it('should add two numbers', function(){
+  //     let $scope = {};
+  //     let controller = $controller('SampleController', {$scope: $scope});
+  //
+  //     $scope.sum(1, 2);
+  //     expect($scope.result).toBe(3);
+  //   });
+  // });
+
+});
